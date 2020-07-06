39bbcb5d32a62e9f9df45ca3531dd51632409eee
Ray Wright <raywright88@gmail.com>
2020-07-06 09:01:45 -0400

    Use responsive layout

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(
+    __name__,
+    external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"],
+    meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
+)
 server = app.server
 
 DF = pd.read_csv("data/us.csv")
