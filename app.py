5aef43ea0dad37b9fc2768231728d52003068550
Ray Wright <raywright88@gmail.com>
2020-07-25 08:02:54 -0400

    Update data

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ INITIAL_COUNTRIES = ["united states"]
 
 NOTES = dcc.Markdown(
     """
-Last updated July 24.
+Last updated July 25.
 Cases and deaths data available for countries, US states, and US counties.
 Tests data available for US states.
 
