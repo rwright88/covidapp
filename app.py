7f5857d2ad25bc283a5e0766432f91bd5796f8d7
Ray Wright <raywright88@gmail.com>
2020-10-03 12:15:27 -0400

    Reduce data frame size using category dtype

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ def read_data(path):
     ]
     df = pd.read_csv(path)
     df["date"] = pd.to_datetime(df["date"])
+    df["name"] = df["name"].astype("category")
+    df["type"] = df["type"].astype("category")
     df = df[cols]
     return df
 
